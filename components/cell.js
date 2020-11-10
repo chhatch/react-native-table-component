7f5968386d61fe16551f1e3ff64b9006c5078868
CJ Hatch <siege.hatch@gmail.com>
2020-11-10 08:09:19 -0600

    Another fix

--- components/cell.js
+++ components/cell.js
@@ -5,7 +5,7 @@ import { View, ViewPropTypes, Text, StyleSheet } from 'react-native';
 export class Cell extends Component {
   static propTypes = {
     style: PropTypes.any,
-    textStyle: Text.propTypes.style,
+    textStyle: PropTypes.any,
     borderStyle: PropTypes.any,
   };
 
